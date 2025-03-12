d88c58576e1ea2d9c9d88544576ebb387b64c60e
TitoCalleros <mirna.molina@gmail.com>
2025-03-11 18:09:16 -0600

    Configuración inicial del proyecto.
    Configuración de tailwind y DaisyUI.
    Creación de estructura de directorios.
    Creación de rutas usando lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import BasicPageComponent from './pages/basic-page/basic-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes básicos',
+    loadComponent: () => import('./pages/basic-page/basic-page.component'),
+  },
+  {
+    path: 'numbers',
+    title: 'Pipes numéricos',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page.component'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  },
+];
