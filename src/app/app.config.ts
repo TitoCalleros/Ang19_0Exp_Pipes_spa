5da767b1646aa0eb306b7adf34d7352dece98999
TitoCalleros <mirna.molina@gmail.com>
2025-03-12 20:34:52 -0600

    Definición y registro de 2 locale_ID adicionales (es-MX y fr). Solo se puede utilizar uno a la vez

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+
+import localeES from '@angular/common/locales/es-MX';
+import localeFR from '@angular/common/locales/fr';
+
+registerLocaleData(localeES, 'es');
+registerLocaleData(localeFR, 'fr');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    // Inyección del LOCALE_ID mediante un alias
+    {
+      provide: LOCALE_ID,
+      useValue: 'es',
+    },
+  ]
 };
