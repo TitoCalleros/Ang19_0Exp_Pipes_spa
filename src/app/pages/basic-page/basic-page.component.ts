78c250664899d1b2ee60dec6bf57066d30cdc4a2
TitoCalleros <mirna.molina@gmail.com>
2025-03-12 19:17:40 -0600

    Se implementa el datePipe con diferentes formatos y GMT.
    Se crea una función para implementar un interval que permita actualizar el valor de la señal de la fecha cada segundo.

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html'
 })
 export default class BasicPageComponent {
@@ -11,5 +11,21 @@ export default class BasicPageComponent {
   nameLower = signal('tito manuel');
   nameUpper = signal('TITO MANUEL');
   fullName = signal('tItO mANUeL');
+
+  customDate = signal( new Date() );
+
+  tickingDateEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+
+    }, 1000);
+
+    // Función que permite limpiar el intervalo al salir de la página.
+    onCleanUp(() => {
+      clearInterval(interval);
+    });
+  });
+
+
 
 }
