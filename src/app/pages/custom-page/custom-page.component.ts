1ecda1aaa606bf145751f03343c98f9a95756901
TitoCalleros <mirna.molina@gmail.com>
2025-03-13 17:29:29 -0600

    Creación de la interfaz Hero, del archivo de datos y preparación de la tabla para mostrar la información en bruto.

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { toggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
 
 @Component({
   selector: 'app-custom-page',
@@ -12,4 +13,6 @@ export default class CustomPageComponent {
   name = signal('Tito Manuel Calleros');
 
   upperCase = signal(true);
+
+  heroes = signal(heroes);
 }
